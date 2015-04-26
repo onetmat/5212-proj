a3e320e2c94e9a478d0f4897ce80bd20437fcda3
Mathew Anderson <msar4c@mst.edu>
2015-04-26 15:00:28 -0500

    Enhanced output of query script.

--- weather-proj/make_queries.sh
+++ weather-proj/make_queries.sh
@@ -8,9 +8,13 @@ file="$1/city_list"
 datefile=$2
 exec 4<$file
 
+rm -rvf $1/queries
+mkdir -v $1/queries
+
 while read -u4 city ; do
    exec 5<$datefile
+   rm -rvf $1/queries/$city
    while read -u5 date ; do
-      echo history_$date/q/$1/$city.json
+      echo history_$date/q/$1/$city.json >> $1/queries/$city
    done
 done
