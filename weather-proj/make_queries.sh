b96597a5edc6d4b73fb5c31dbca36b34dac331d5
Mathew Anderson <msar4c@mst.edu>
2015-04-26 20:19:39 -0500

    Amended query script to scrape WUnderground for data.

--- weather-proj/make_queries.sh
+++ weather-proj/make_queries.sh
@@ -8,13 +8,23 @@ file="$1/city_list"
 datefile=$2
 exec 4<$file
 
+# Clear out query directory to make new queries
 rm -rvf $1/queries
 mkdir -v $1/queries
+
+# Clear out data directory
+rm -rvf $1/data
+mkdir -v $1/data
 
 while read -u4 city ; do
    exec 5<$datefile
    rm -rvf $1/queries/$city
+
+   rm -rvf $1/data/$city
+   mkdir $1/data/$city
    while read -u5 date ; do
+      querystr="history_$date/q/$1/$city.json"
       echo history_$date/q/$1/$city.json >> $1/queries/$city
+      curl http://api.wunderground.com/api/5b0d8023c31797a8/$querystr > $1/data/$city/$date.json
    done
 done
